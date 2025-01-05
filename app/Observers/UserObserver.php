70cc1e93a02d2431405819a87769a3505499fda0
blackcloud2025 <s4ul2025@gmail.com>
2025-01-04 19:15:01 -0600

    creacion de observabor encargado de borrar productos de usuarios eliminados

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use App\Models\Product;
+use Illuminate\Support\Facades\Storage;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "updated" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //borra los productos de usuarios borrados
+        
+        // Obtener todos los productos del usuario
+        $products = Product::where('user_id', $user->id)->get();
+
+        foreach ($products as $product) {
+            // Eliminar imágenes
+            foreach ($product->images as $image) {
+                Storage::disk('public')->delete($image->image_path);
+                $image->delete();
+            }
+            
+            // Eliminar producto
+            $product->delete();
+        }
+    }
+   
+}
+
+
