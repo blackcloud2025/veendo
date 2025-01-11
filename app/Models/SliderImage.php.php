e2ee55f9349ad32056c301ec2d908d4af027aedb
blackcloud2025 <s4ul2025@gmail.com>
2025-01-10 18:27:32 -0600

    creacion de crud para imagenes de publicistas para control de imagernes

---
+++ app/Models/SliderImage.php.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SliderImage extends Model
+{
+    protected $fillable = [
+        'image_path',
+        'title',
+        'description',
+        'order',
+        'active'
+    ];
+}
